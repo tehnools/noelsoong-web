d4ea6b5f5ca04f2811e6629c41a65fe678197e50
noel <noelsoong@live.com>
2019-11-21 13:55:42 +1300

    WIP: biocard

--- src/components/BioCard.jsx
+++ src/components/BioCard.jsx
@@ -1,25 +1,47 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from '../App'
-import { Card, CardMedia, CardContent, Button, makeStyles, Typography } from "@material-ui/core";
+import React from 'react'
+import { Card, CardMedia, CardContent, Button, makeStyles, Typography, Box} from "@material-ui/core";
+import { useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-
+    header:{
+        fontFamily: 'Roboto',
+        fontSize: '2rem',
+        fontWeight: 700
+    },
+    header2: {
+        fontFamily: 'Roboto',
+        fontSize: '1.7rem',
+        fontWeight: 300
+    },
+    media: {
+        height: '360px',
+        maxWidth: '360px'
+    },
+    card: {
+        maxWidth: '360px'
+    },
+    cardContent: {
+        display: 'flex',
+        flexAlign: 'start'
+    }
 })
 
 
 
 export default function BioCard() {
-    const theme = useContext(ThemeContext)
     const classes = useStyles()
+    const theme = useTheme()
     return (
-        <Card>
-            <CardMedia title="" image="/hs.jpg" />
-            <CardContent>
-                <Typography>
-                    Noel Soong
-                </Typography>
-                <Typography>
-                    tehnools
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} title="noelsoong" image='/static/images/hs.jpg' />
+            <CardContent >
+                <Typography color="primary">
+                    <Box className={classes.header} textAlign='left'>
+                        Noel Soong
+                    </Box>
+                    <Box className={classes.header2} textAlign='left'>
+                        tehnools
+                    </Box>
                 </Typography>
             </CardContent>
         </Card>
