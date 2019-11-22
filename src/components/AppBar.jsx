284eec6dd7ffc3ee055b8fbe15a014f65b570988
noel <noelsoong@live.com>
2019-11-22 22:01:57 +1300

    app bar

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bar from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography'
+
+export default function AppBar () {
+  return (
+    <Bar position="static">
+      <Typography variant="h2">
+      N
+      </Typography>
+    </Bar>
+  )
+}
