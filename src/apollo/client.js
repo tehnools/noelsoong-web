a3e9e95d7ce654f4f98b8beb40f3a3e728ed9975
noel <noelsoong@live.com>
2019-11-24 17:32:17 +1300

    Configure token

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,10 +1,15 @@
 import ApolloClient from 'apollo-boost'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+if(!process.env.GITHUB_TOKEN) {
+  console.error('Make sure you configure your token to use Apollo!');
+  
+}
+
 export const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: 'Bearer 12784d30d12cdc0bba4e17daf548f10ed9f63abc'
+    Authorization: 'Bearer ' + process.env.GITHUB_TOKEN
   },
   cache: new InMemoryCache()
 })
