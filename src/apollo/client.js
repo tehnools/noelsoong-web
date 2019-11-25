d28d19ce93d8ac2da2e31d206828fe286f624dad
noel <noelsoong@live.com>
2019-11-25 13:28:36 +1300

    set default bearer auth

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,15 +2,15 @@ import ApolloClient from 'apollo-boost'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 require('dotenv').config()
 
-if(!process.env.GITHUB_TOKEN) {
-  console.error('Make sure you configure your token to use Apollo!');
-  
-}
-
 export const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
-  headers: {
-    Authorization: 'Bearer ' + process.env.GITHUB_TOKEN
+  request: operation => {
+    const token = localStorage.getItem('token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `${token}` : ''
+      }
+    })
   },
   cache: new InMemoryCache()
 })
