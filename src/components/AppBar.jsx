91bb85e727f35eb09d674689db177479da58521c
noel <noelsoong@live.com>
2019-12-04 14:56:42 +1300

    Clear Appbar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import Container from '@material-ui/core/Container'
+import {
+  Container,
+  makeStyles
+} from '@material-ui/core'
 import Bar from '@material-ui/core/AppBar'
 import Typography from '@material-ui/core/Typography'
 
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    zIndex: 999,
+    position: 'absolute',
+    boxShadow: 'none',
+    backgroundColor: '#00000000',
+    top: theme.spacing(4)
+  },
+  fixed: {
+    position: 'fixed',
+    top: 0
+  },
+  header: {
+    textShadow: '1px 1px 1px #00000075',
+    fontFamily: 'Fredoka One'
+  }
+}))
+
 export default function AppBar () {
+  const classes = useStyles()
+
   return (
-    <Bar position="static">
+    <Bar className={classes.appBar} position="static">
       <Container>
-        <Typography variant="h2" component='h2'>
-          N
+        <Typography className={classes.header} variant="h2" component='h2'>
+          N.
         </Typography>
       </Container>
     </Bar>
