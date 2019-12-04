7fae4a8fce85f3d1d207368f1f0977031cbe8ef7
noel <noelsoong@live.com>
2019-12-04 22:02:49 +1300

    Project Grid Tile

---
+++ src/components/Project.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {
+  GridListTile,
+  GridListTileBar,
+  Link,
+  IconButton,
+  makeStyles
+} from '@material-ui/core'
+
+import GitHubIcon from '@material-ui/icons/GitHub'
+
+const useStyles = makeStyles(theme => ({
+  tileImage: {
+    height: '100%',
+    width: 300,
+    '&:hover': {
+      filter: 'grayscale(80%) blur(3px);',
+      '-web-kit-filter-': 'grayscale(80%) blur(50%)'
+    },
+    transition: 'filter 0.5s'
+  },
+  githubIcon: {
+    color: 'white'
+  }
+})
+)
+
+export default function Project (props) {
+  const classes = useStyles()
+  const {
+    name,
+    description,
+    imgUrl,
+    link,
+    github
+  } = props.project
+  return (
+    <GridListTile
+      style={{ height: 'auto', overflow: 'hidden' }}
+    >
+      <Link href={link}>
+        <img
+          className={classes.tileImage}
+          src={imgUrl}
+          alt={name}
+        />
+      </Link>
+      <GridListTileBar
+        title={<Link href={link} style={{ color: 'white' }}>{name}</Link>}
+        actionIcon={
+          <IconButton href={github}>
+            <GitHubIcon className={classes.githubIcon} />
+          </IconButton>
+        }
+      />
+    </GridListTile>
+  )
+}
