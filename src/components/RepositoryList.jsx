d0b32a72d3fd577f282d1497006b174993e8271c
noel <noelsoong@live.com>
2019-12-04 14:57:03 +1300

    Refactor: repsitory list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import {
   Box,
   Grid,
+  Switch,
+  Typography,
+  Collapse,
+  FormControlLabel,
   makeStyles
 } from '@material-ui/core'
 import { useQuery } from '@apollo/react-hooks'
@@ -37,25 +41,64 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     padding: theme.spacing(1)
+  },
+  headerBox:
+  {
+    display: 'flex'
+  },
+  header3: {
+    marginTop: theme.spacing(2),
+    color: '#636363'
+  },
+  formControl: {
+    marginLeft: theme.spacing(2)
   }
 }))
 
 export default function RepositoryList (props) {
   const classes = useStyles()
+  const [hide, setHide] = React.useState(true)
   const { data, loading, error } = useQuery(QUERY_GITHUB_REPOS)
-  if (error) return `Error! ${error}`
+
+  const handleCheck = () => {
+    setHide(prev => !prev)
+  }
+
+  if (error) {
+    return `Error! ${error}`
+  }
 
   return (
-    loading && data === null ? props.fallback()
-      : <Box className={classes.root}>
-        <Grid container
-          alignItems='flex-start'
-          wrap='wrap'
-          xl='auto'
-          spacing={1}
-        >
-          {data && data.user.repositories.nodes.map(repo => <RepositoryCard key={JSON.stringify(repo)} repo={repo} />)}
-        </Grid>
+    <>
+      <Box className={classes.headerBox} >
+        <Typography
+          variant='h3'
+          className={classes.header3}>
+            Repositories
+          <FormControlLabel
+            className={classes.formControl}
+            control={<Switch checked={!hide}
+              onChange={handleCheck} />}
+            label='Hide'
+          />
+        </Typography>
       </Box>
+      {
+        loading && data === null
+          ? props.fallback()
+          : <Collapse in={hide}>
+            <Box className={classes.root}>
+              <Grid container
+                alignItems='flex-start'
+                wrap='wrap'
+                xl='auto'
+                spacing={1}
+              >
+                {data && data.user.repositories.nodes.map(repo => <RepositoryCard key={JSON.stringify(repo)} repo={repo} />)}
+              </Grid>
+            </Box>
+          </Collapse>
+      }
+    </>
   )
 }
