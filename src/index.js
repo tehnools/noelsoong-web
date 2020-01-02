0f9aa117bb6269b904ddb0703ad8bdce8c8861f0
noel <noelsoong@live.com>
2020-01-02 14:39:42 +1300

    :wastebasket: Remove Apollo client

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { ApolloProvider } from '@apollo/react-hooks'
-import { client } from './apollo/client'
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <Route exact path='/' >
-        <App/>
-      </Route>
-    </Router>
-  </ApolloProvider>, document.getElementById('root'))
+  <Router>
+    <Route exact path='/' >
+      <App/>
+    </Route>
+  </Router>
+  , document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
